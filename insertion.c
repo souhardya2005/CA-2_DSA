845b4190134ecfe03c3662a5d07ff04eb050155e
souhardya2005 <souhardyaskhn@gmail.com>
2024-09-11 22:39:45 +0530

    Add files via upload

---
+++ insertion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void insert(int arr[], int *n, int pos, int value) {
+    
+    for (int i = *n - 1; i >= pos; i--) {
+        arr[i + 1] = arr[i];
+    }
+   
+    arr[pos] = value;
+    /
+    (*n)++;
+}
+
+int main() {
+    int arr[100] = {1, 2, 3, 4, 5};  
+    int n = 5; 
+    int pos = 2; 
+    int value = 10; 
+
+    printf("Array before insertion: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    insert(arr, &n, pos, value); // Calling the insert function
+
+    printf("\nArray after insertion: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
