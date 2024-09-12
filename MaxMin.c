03c33b8ad1591cb26c31b80958544e897ec8004b
souhardya2005 <souhardyaskhn@gmail.com>
2024-09-12 20:58:05 +0530

    Add files via upload

---
+++ MaxMin.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void findMaxMin(int arr[], int n, int *max, int *min) {
+    *max = arr[0];  
+    *min = arr[0];  
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > *max) {
+            *max = arr[i];
+        }
+        if (arr[i] < *min) {
+            *min = arr[i];
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max, min;
+    findMaxMin(arr, n, &max, &min);
+
+    printf("Maximum element: %d\n", max);
+    printf("Minimum element: %d\n", min);
+
+    return 0;
+}
